16cf5faa57af7bd61e3ad1d68ad33f298a900e26
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-07-13 12:37:47 -0400

    upload to heroku

--- movie_trailer_review/app.py
+++ movie_trailer_review/app.py
@@ -1,3 +1,25 @@
+#sklearn and ML
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelBinarizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from wordcloud import WordCloud,STOPWORDS
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize,sent_tokenize
+from bs4 import BeautifulSoup
+import spacy
+import re,string,unicodedata
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.stem import LancasterStemmer,WordNetLemmatizer
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from textblob import TextBlob
+from textblob import Word
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+import nltk
+
 # import necessary libraries
 import os
 from flask import (
@@ -7,22 +29,49 @@ from flask import (
     request,
     redirect)
 
-
-app = Flask(__name__)
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Session
+
+# Imports the method used for connecting to DBs
+from sqlalchemy import create_engine
+
+# Imports the methods needed to abstract classes into tables
+from sqlalchemy.ext.declarative import declarative_base
+
+# Allow us to declare column types
+from sqlalchemy import Column, Integer, String, Float 
+
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/reviews.sqlite"
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 
 db = SQLAlchemy(app)
 
-from .models import Review
+# Base = automap_base()
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(255))
+    sentiment = db.Column(db.Integer)
+
+# engine = create_engine("sqlite:///db/reviews.sqlite")
+# conn = engine.connect()
+
+# Base.prepare(engine, reflect=True) 
+# Review = Base.classes.review
+# session = Session(bind=engine) 
+
 
 @app.before_first_request
 def setup():
     # Recreate database each time for demo
-    # db.drop_all()
+    db.drop_all()
     db.create_all()
 
 # create route that renders index.html template
@@ -35,11 +84,9 @@ def home():
 @app.route("/send", methods=["GET", "POST"])
 def send():
     if request.method == "POST":
-        first = request.form["first"]
-        second = request.form["second"]
-        third = request.form["third"]
-
-        review = Review(first=first, second=second, third=third)
+        review = request.form["review"]
+                
+        review = Review(id=id, review=review)
         db.session.add(review)
         db.session.commit()
         return redirect("/", code=302)
@@ -47,32 +94,44 @@ def send():
     return render_template("form.html")
 
 
-
 @app.route("/api/reviews")
-def wordcloud():
+def clean_review():
+    
+     #Join all review words
+    results = db.session.query(Review).all()
 
-    #Define stopwords
-    stopwords = ['and', 'bad', 'good','br', 'film', 'movie', 'about', 'above', 'across', 'after', 'afterwards']
+    id = [result[0] for result in results]
+    review_words = [result[1] for result in results]
+    sentiment = [result[2] for result in results]
 
-    #Join all review words
-    review_words = db.session.query(Review.first).all()
+    #Tokenization of text
+    import nltk
+    nltk.download('stopwords')
+    tokenizer=ToktokTokenizer()
+    #Setting English stopwords
+    stopword_list=nltk.corpus.stopwords.words('english')
+   
+    def remove_special_characters(text, remove_digits=True):
+        pattern=r'[^a-zA-z0-9\s]'
+        text=re.sub(pattern,'',text)
+        return text
+        #Apply function on review column
+        review_words=review_words.apply(remove_special_characters)
+    
+    #removing the stopwords
+    def remove_stopwords(text, is_lower_case=False):
+        tokens = tokenizer.tokenize(text)
+        tokens = [token.strip() for token in tokens]
+        if is_lower_case:
+            filtered_tokens = [token for token in tokens if token not in stopword_list]
+        else:
+            filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+            filtered_text = ' '.join(filtered_tokens)    
+        return filtered_text
 
-    # Generate a word cloud image based on bag of popcorn
-    mask = np.array(Image.open("bag.jpg"))
-
-    wordcloud = WordCloud(width = 800, height = 800, 
-                background_color ='white', 
-                stopwords = stopwords, 
-                min_font_size = 10, 
-                max_words=200,
-                mask=mask).generate(review_words) 
-  
-    # plot the WordCloud image                        
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud, interpolation="bilinear") 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
-    plt.show()
+        #Apply function on review column
+        review_words=review_words.apply(remove_stopwords)
+    
 
 if __name__ == "__main__":
     app.run()
--- movie_trailer_review/models.py
+++ movie_trailer_review/models.py
@@ -1,14 +1,21 @@
-from .app import db
+# from sqlalchemy import create_engine
 
+# from sqlalchemy.ext.declarative import declarative_base
 
-class Review(db.Model):
-    __tablename__ = 'reviews'
+# from sqlalchemy import Column, Integer, String, Float 
 
-    id = db.Column(db.Integer, primary_key=True)
-    first = db.Column(db.String(64))
-    second = db.Column(db.String(64))
-    third = db.Column(db.String(64))
+# # Sets an object to utilize the default declarative base in SQL Alchemy
+# Base = declarative_base()
 
-    def __repr__(self):
-        return '<Review %r>' % (self.first)
+# class Review(Base):
+#     __tablename__ = 'reviews'
 
+#     id = Column(Integer, primary_key=True)
+#     review = Column(String(255))
+    
+
+# engine = create_engine("sqlite:///db/reviews.sqlite")
+# conn = engine.connect()
+
+# Base.metadata.create_all(engine)
+# # Base.metadata.drop_all(engine) 
