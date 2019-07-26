8803888c15891473e17ac143f11a5f23800b8977
Victor Yamaykin <victor.yamaykin@gmail.com>
2019-07-26 19:42:14 -0400

    final

--- initdb.py
+++ initdb.py
@@ -2,3 +2,4 @@ from movie_trailer_review.app import db
 
 # db.drop_all()
 db.create_all()
+exit()
--- movie_trailer_review/app.py
+++ movie_trailer_review/app.py
@@ -44,120 +44,67 @@ from flask import (
 
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
 # DATABASE_URL will contain the database connection string:
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///reviews.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///reviews.sqlite"
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "reviews.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 # Connects to the database using the app config
 db = SQLAlchemy(app)
 
-engine = create_engine("sqlite:///reviews.sqlite")
-# c = engine.connect()
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-Base.metadata.create_all(engine)
-
 class Review(db.Model):
-    __tablename__ = 'reviews'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    review = db.Column(db.String(255))
-    # prediction = db.Column(db.String(255))
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    review = db.Column(db.String(80), unique=True, nullable=False, primary_key=False)
+    def __repr__(self):
+        return "<Review: {}>".format(self.review)
 
-# Save references to each table
-Reviews = Base.classes.reviews
-
-# prepare session to receive user inputs
-session = Session(engine)
-
-@app.before_first_request
-def setup():
-    # Recreate database each time for demo
-    db.drop_all()
-    db.create_all()
-
-# create route that renders index.html template
 @app.route("/")
 def home():
+    engine = create_engine("sqlite:///reviews.sqlite")
 
     table = engine.execute("SELECT * FROM reviews").fetchall()
-    print(type(table))
+    table = Review.query.all()
+
     return render_template("index.html", table=table)
 
-@app.route("/reviews")
-def reviews():
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Reviews).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the reviews
-    return jsonify(df)
-
-
-    #this is how Naive Bayes classifier expects the input
-# def create_word_features(words):
-#         #Join all review words
-#         results = engine.execute("SELECT * FROM reviews").fetchall()
-
-#         text = [result[1] for result in results]
-#         print(text)
-#         useful_words = [word for word in words if word not in stopwords.words("english")]
-#         my_dict = dict([(word, True) for word in useful_words])
-#         return my_dict 
-
-#         neg_reviews = []
-#         for fileid in movie_reviews.fileids('neg'):
-#             words = movie_reviews.words(fileid)
-#             neg_reviews.append((create_word_features(words),"negative"))
-#         print(neg_reviews[0])
-#         print(len(neg_reviews))
-
-#         pos_reviews = []
-#         for fileid in movie_reviews.fileids('pos'):
-#             words = movie_reviews.words(fileid)
-#             pos_reviews.append((create_word_features(words),"positive"))
-#         #print(pos_reviews[0])
-#         print(len(pos_reviews))
-
-#         train_set = neg_reviews[:750] +  pos_reviews[:750]
-#         test_set = neg_reviews[750:] + pos_reviews[750:]
-#         print(len(train_set), len(test_set))
-
-#         #train the classifier
-#         classifier = NaiveBayesClassifier.train(train_set)
-
-#         #find accuracy percentage
-#         accuracy = nltk.classify.util.accuracy(classifier,test_set)
-#         print(accuracy * 100)
-
-#         words = word_tokenize(text)
-#         words = create_word_features(words)
-
-#         results = classifier.classify(words)
-
-#         print(results)
-#         predictions = [result for result in results]
-#         print(predictions)
-#         df['prediction'] = predictions
-
-#         return jsonify(df)
-
-# Query the database and send the jsonified results
 @app.route("/send", methods=["GET", "POST"])
 def send():
+    engine = create_engine("sqlite:///reviews.sqlite")
+
+    session = Session(engine)
     if request.method == "POST":
-        review = request.form["review"]
-        review = Review(review=review)
+        review = Review(review=request.form.get("review"))
         db.session.add(review)
         db.session.commit()
         return redirect("/", code=302)
 
-    return render_template("form.html")
+    table = Review.query.all()
+
+    return render_template("form.html", table=table)
+
+@app.route("/update", methods=["POST"])
+def update():
+    newreview = request.form.get("newreview")
+    oldreview = request.form.get("oldreview")
+    review = Review.query.filter_by(review=oldreview).first()
+    review.review = newreview
+    db.session.commit()
+    return redirect("/", code=302)
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    review = request.form.get("review")
+    review = Review.query.filter_by(review=review).first()
+    db.session.delete(review)
+    db.session.commit()
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run()
